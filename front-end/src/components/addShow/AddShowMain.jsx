812df67f230aa6b5ed150c4c34f9cffc64e2cd0b
Kai-Animator <kaireml@protonmail.com>
2023-02-17 16:01:31 +0900

    added addnew changes

--- front-end/src/components/addShow/AddShowMain.jsx
+++ front-end/src/components/addShow/AddShowMain.jsx
@@ -1,30 +1,17 @@
 function AddShowMain() {
 
-  async function getShowID(name) {
-    const data = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=22232a34b1256a41ee95dfdb04aa1810&language=en-US&query=warcraft&page=1&include_adult=false`
-    );
-    const dataTreated = await data.json();
-    if (dataTreated.results.lenthth > 1) {
-      for (let movie of dataTreated.results) {
-        if (movie.name == name) {
-          return movie.id;
-        }
-      }
-    }
-  }
-
-  async function getShowImage(name) {
-    let id = await getShowID(name);
-    let posterAdd = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=22232a34b1256a41ee95dfdb04aa1810&language=en-US&query=${id}&page=1&include_adult=false`
-    );
-    let posterAddTreated = posterAdd.json();
-    return `https://image.tmdb.org/t/p/original/${posterAddTreated}`;
-  }
+  // async function getShowImage(name) {
+  //   let id = await getShowID(name);
+  //   let posterAdd = await fetch(
+  //     `https://api.themoviedb.org/3/search/movie?api_key=22232a34b1256a41ee95dfdb04aa1810&language=en-US&query=${id}&page=1&include_adult=false`
+  //   );
+  //   let posterAddTreated = posterAdd.json();
+  //   return `https://image.tmdb.org/t/p/original/${posterAddTreated}`;
+  // }
+  //
 
   return (
-    <div>
+    <div className="addshow-main">
       {/* Map on through fetch object and create a div for each object in there */}
     </div>
   );
--- front-end/src/components/addShow/AddShowNavbar.jsx
+++ front-end/src/components/addShow/AddShowNavbar.jsx
@@ -1,17 +1,45 @@
-function AddShowNavbar() {
+import { useEffect, useRef, useState } from 'react';
+
+function AddShowNavbar({ setSearchList }) {
+  const [showlist, setShowlist] = useState([]);
+
+  async function getShowID(name) {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/search/tv?api_key=22232a34b1256a41ee95dfdb04aa1810&language=en-US&query=${name}&page=1&include_adult=false`
+    ).then((data) => data.json());
+    await setShowlist(data.results);
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const searchInput = document.getElementsByClassName('find-show');
+    getShowID(searchInput[0].value);
+  };
+
+  useEffect(() => {
+    const showArray = showlist.reduce((lastArray, show) => {
+      const showObject = {
+        name: show.name,
+        show_id: show.id,
+        image: show.poster_path,
+      };
+      lastArray.push(showObject);
+      return lastArray;
+    }, []);
+    setSearchList(showArray)
+  }, [showlist]);
+
   return (
     <div className='addshow-nav'>
       <form action='/'>
         <input
           type='text'
           name='find-show'
-          id='find-show'
+          className='find-show'
           placeholder='Search Shows'
           required
         />
-        <button type='submit' href='/'>
-          Search
-        </button>
+        <button onClick={handleClick}>Search</button>
       </form>
     </div>
   );
