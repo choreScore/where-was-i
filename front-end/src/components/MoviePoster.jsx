a1a6e473338755c1b28290219bec437795206117
Kai-Animator <kaireml@protonmail.com>
2023-02-17 09:28:00 +0900

    adjusted homepage & moviePoster files

--- front-end/src/components/MoviePoster.jsx
+++ front-end/src/components/MoviePoster.jsx
@@ -1,27 +1,13 @@
-function MoviePoster({setDisplay, showname, display}) {
+import '../styles/movie-poster.css';
 
-  function showButton(e) {
-    e.preventDefault();
-    setDisplay('show_button');
-  }
-
-  function hideButton(e) {
-    e.preventDefault();
-    setDisplay('hide_button');
-  }
-
+function MoviePoster({ showname }) {
   return (
-    <div className='cards' onMouseEnter={showButton} onMouseLeave={hideButton}>
-      <img
-        className='card-img'
-        src={require('../test-image/sopranos.jpg')}
-        alt={showname}
-      />
-      <div className='card-text'>
-        <h3>{showname}</h3>
+    <div className='cards'>
+      <div className='image-container'>
+        <div className='image'/>
       </div>
-      <div className={display}>
-        <button id='progress'>View Progress</button>
+      <div className='text-container'>
+        <h3>{showname}</h3>
       </div>
     </div>
   );
