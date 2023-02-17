5f21ed99524ed2fa5cb20e0dcbf53bd0b063372f
eric nicolas <eric.e.nicolas@gmail.com>
2023-02-17 17:25:03 +0900

    done with the new post

--- front-end/src/components/addShow/AddShowSideBar.jsx
+++ front-end/src/components/addShow/AddShowSideBar.jsx
@@ -1,20 +1,54 @@
-function AddShowSideBar() {
+import { useEffect, useRef, useState } from 'react';
+
+function AddShowSideBar(props) {
+  const [seasonSelect, setSeasonSelectd] = useState('');
+  const [episodeSelect, setpisodeSelected] = useState('');
+
+async function updateDatabase(){
+    const obj = {
+      "user_id":localStorage.user_id,
+      "show_id":props.show_id,
+      "showname":props.name,
+      "season":seasonSelect,
+      "episode":episodeSelect,
+      "image":`https://image.tmdb.org/t/p/original${props.image}`
+    }
+
+    await fetch('http://localhost:4000/user/shows', {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          body: JSON.stringify(obj),
+          
+        });
+}
+ 
+
   return (
     <div className="addshow-side">
-      <form action='/'>
+      <form action={updateDatabase}>
         <input
-          type='number'
+          className="addShow"
+          type='text'
           name='Season'
           id='season'
           placeholder='season'
+          value={seasonSelect}
           required
+          onChange={(event) =>setSeasonSelectd(event.target.value)}
+          
         />
         <input
-          type='number'
+          className="addShow"
+          type='text'
           name='Episode'
           id='episode'
           placeholder='episode'
+          value={episodeSelect}
           required
+          onChange={(event) =>setpisodeSelected(event.target.value)}
         />
         <button type='submit' href='/'>
           Submit
