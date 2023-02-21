24ac5917856a03a5059a05da016cb4a99f2515ed
Harry.Cleveland <109498948+hccleveland@users.noreply.github.com>
2023-02-21 10:51:17 +0900

    feat: Implement genre dropdown

--- front-end/src/components/addShow/AddShowNavbar.jsx
+++ front-end/src/components/addShow/AddShowNavbar.jsx
@@ -3,6 +3,15 @@ import { useEffect, useRef, useState } from "react";
 function AddShowNavbar({ setSearchList }) {
   const [showlist, setShowlist] = useState([]);
 
+  getGenre();
+
+  async function getGenre() {
+    const genreData = await fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=22232a34b1256a41ee95dfdb04aa1810`
+    ).then((genreData) => genreData.json());
+    console.log(genreData);
+  }
+
   async function getShowID(name) {
     const data = await fetch(
       `https://api.themoviedb.org/3/search/tv?api_key=22232a34b1256a41ee95dfdb04aa1810&language=en-US&query=${name}&page=1&include_adult=false`
@@ -34,6 +43,32 @@ function AddShowNavbar({ setSearchList }) {
 
   return (
     <div className="addshow-nav">
+      <form action="/">
+        <select id="genre" name="Genre">
+          <option value="Action">Action</option>
+          <option value="Adventure">Adventure</option>
+          <option value="Animation">Animation</option>
+          <option value="Comedy">Comedy</option>
+          <option value="Crime">Crime</option>
+          <option value="Documentary">Documentary</option>
+          <option value="Drama">Drama</option>
+          <option value="Family">Family</option>
+          <option value="Fantasy">Fantasy</option>
+          <option value="History">History</option>
+          <option value="Horror">Horror</option>
+          <option value="Music">Music</option>
+          <option value="Mystery">Mystery</option>
+          <option value="Romance">Romance</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="TV Movie">TV Movie</option>
+          <option value="Thriller">Thriller</option>
+          <option value="War">War</option>
+          <option value="Western">Western</option>
+        </select>
+        <button onClick={handleClick} className="btn">
+          Genre
+        </button>
+      </form>
       <form action="/">
         <input
           type="text"
