be275963e1ad9b8272f5aae6f437eaf7269445b3
amekatze <setharie@gmail.com>
2023-02-22 10:32:04 +0900

    styling: Restructuring movie poster in add show and home

--- front-end/src/components/addShow/SearchPoster.jsx
+++ front-end/src/components/addShow/SearchPoster.jsx
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react";
 
 function SearchPoster(props) {
   const [countryFlag, setCountryFlag] = useState();
+  const showName =
+    props.show.name.length > 30
+      ? props.show.name.slice(0, 19) + " ..."
+      : props.show.name;
   function sendBack() {
     props.setShowSelected(props.show);
   }
@@ -25,10 +29,12 @@ function SearchPoster(props) {
         />
       </div>
       <div className="search-text-container">
-        <h3>{props.show.name}</h3>
+        <h3>{showName}</h3>
+        <div className="rating-and-flag">
+          <img src={`https://flagcdn.com/w20/${countryFlag}.png`} width={20} />
+          <p>Rating {props.show.vote_average}/10</p>
+        </div>
       </div>
-      <h4>rating:{props.show.vote_average}</h4>
-      <img src={`https://flagcdn.com/w20/${countryFlag}.png`} width={20} />
     </div>
   );
 }
